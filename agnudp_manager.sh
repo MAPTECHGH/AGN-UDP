031d1b05d7020f41e13d2849fe1a0be594f3d443
MAPTECHGH <138830522+MAPTECHGH@users.noreply.github.com>
2024-07-26 21:48:39 +0000

    Update agnudp_manager.sh

--- agnudp_manager.sh
+++ agnudp_manager.sh
@@ -8,15 +8,18 @@ SYSTEMD_SERVICE="/etc/systemd/system/hysteria-server.service"
 mkdir -p "$CONFIG_DIR"
 touch "$USER_DB"
 
+
 fetch_users() {
     if [[ -f "$USER_DB" ]]; then
-        sqlite3 "$USER_DB" "SELECT password FROM users;" | paste -sd, -
+        sqlite3 "$USER_DB" "SELECT '\"' || username || '\":\"' || password || '\"' FROM users;" | paste -sd, -
     fi
 }
 
+
 update_userpass_config() {
-    local users=$(fetch_users | sed 's/,/","/g')
-    jq ".auth.passwords = [\"$users\"]" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
+        local users=$(fetch_users)
+
+    jq ".auth.userpass = { $users }" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
 }
 
 add_user() {
@@ -36,48 +39,11 @@ add_user() {
     fi
 }
 
-edit_user() {
-    echo "Enter username to edit:"
-    read -r username
-    echo "Enter new password:"
-    read -r password
-
-    # Update the user in the database
-    sqlite3 "$USER_DB" "UPDATE users SET password = '$password' WHERE username = '$username';"
-    if [[ $? -eq 0 ]]; then
-        echo "User $username updated successfully."
-        update_userpass_config
-        restart_server
-    else
-        echo "Error: Failed to update user $username."
-    fi
-}
-
-delete_user() {
-    echo "Enter username to delete:"
-    read -r username
-
-    # Delete the user from the database
-    sqlite3 "$USER_DB" "DELETE FROM users WHERE username = '$username';"
-    if [[ $? -eq 0 ]]; then
-        echo "User $username deleted successfully."
-        update_userpass_config
-        restart_server
-    else
-        echo "Error: Failed to delete user $username."
-    fi
-}
-
-show_users() {
-    echo "Current users:"
-    sqlite3 "$USER_DB" "SELECT username FROM users;"
-}
-
 change_domain() {
     echo "Enter new domain:"
     read -r domain
 
-    jq ".server = \"$domain\"" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
+    jq ".server_names = \"$domain\"" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
 
     echo "Domain changed to $domain successfully."
 
@@ -88,7 +54,7 @@ change_obfs() {
     echo "Enter new obfuscation string:"
     read -r obfs
 
-    jq ".obfs.password = \"$obfs\"" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
+    jq ".obfs.salamander.password = \"$obfs\"" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
 
     echo "Obfuscation string changed to $obfs successfully."
 
@@ -100,7 +66,6 @@ change_up_speed() {
     read -r up_speed
 
     jq ".up_mbps = $up_speed" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
-    jq ".up = $up_speed Mbps" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
 
     echo "Upload speed changed to $up_speed Mbps successfully."
 
@@ -112,7 +77,6 @@ change_down_speed() {
     read -r down_speed
 
     jq ".down_mbps = $down_speed" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
-    jq ".down = $down_speed Mbps" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
 
     echo "Download speed changed to $down_speed Mbps successfully."
 
@@ -124,16 +88,13 @@ show_menu() {
     echo " AGNUDP Manager"
     echo "----------------------------"
     echo "1. Add new user"
-    echo "2. Edit user password"
-    echo "3. Delete user"
-    echo "4. Show users"
-    echo "5. Change domain"
-    echo "6. Change obfuscation string"
-    echo "7. Change upload speed"
-    echo "8. Change download speed"
-    echo "9. Restart server"
-    echo "10. Uninstall server"
-    echo "11. Exit"
+    echo "2. Change domain"
+    echo "3. Change obfuscation string"
+    echo "4. Change upload speed"
+    echo "5. Change download speed"
+    echo "6. Restart server"
+    echo "7. Uninstall server"
+    echo "8. Exit"
     echo "----------------------------"
     echo "Enter your choice: "
 }
@@ -177,34 +138,25 @@ while true; do
             add_user
             ;;
         2)
-            edit_user
-            ;;
-        3)
-            delete_user
-            ;;
-        4)
-            show_users
-            ;;
-        5)
             change_domain
             ;;
-        6)
+        3)
             change_obfs
             ;;
-        7)
+        4)
             change_up_speed
             ;;
-        8)
+        5)
             change_down_speed
             ;;
-        9)
+        6)
             restart_server
             ;;
-        10)
+        7)
             uninstall_server
             exit 0
             ;;
-        11)
+        8)
             exit 0
             ;;
         *)
